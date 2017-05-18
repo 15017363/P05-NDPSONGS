e029065b8ef079d45cc1786e2e88be43daa026d4
15017103 <15017103@myrp.edu.sg>
2017-05-18 23:56:57 +0800

    initial commit

--- app/src/main/java/com/example/a15017363/p05ndpsongs/SecondActivity.java
+++ app/src/main/java/com/example/a15017363/p05ndpsongs/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.a15017363.p05ndpsongs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -43,7 +44,6 @@ public class SecondActivity extends AppCompatActivity {
                 Song data = new Song(SongList.get(i).get_id(), SongList.get(i).getTitle(), SongList.get(i).getSingers(), SongList.get(i).getYear(), SongList.get(i).getStars());
                 intent.putExtra("data", data);
                 startActivityForResult(intent, i);
-
             }
         });
 
@@ -68,7 +68,16 @@ public class SecondActivity extends AppCompatActivity {
                                     Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == 9){
+        if (resultCode == RESULT_OK){
+            Log.i("test", "test");
+            SongList.clear();
+            DBHelper db = new DBHelper(SecondActivity.this);
+            ArrayList<Song> song = db.getAllSongs();
+            db.close();
+
+            for (int i = 0; i < song.size(); i++){
+                SongList.add(new Song(song.get(i).get_id(), song.get(i).getTitle(), song.get(i).getSingers(), song.get(i).getYear(),song.get(i).getStars()));
+            }
             caSong.notifyDataSetChanged();
         }
     }
