5bda3b1f966e71870ea150d82eb57424045c2947
dennywongso <15017096@myrp.edu.sg>
2017-05-18 13:34:38 +0800

    song class and main3activity

---
+++ app/src/main/java/com/example/a15017363/p05ndpsongs/Main3Activity.java
@@ -0,0 +1,22 @@
+package com.example.a15017363.p05ndpsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class Main3Activity extends AppCompatActivity {
+    EditText etId, etTitle, etSinger, etYear;
+    Button btnUpdate, btnDelete, btnCancel;
+    RadioGroup rgStars;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        etId = (EditText)findViewById(R.id.etId);
+        etTitle = (EditText)findViewById(R.id.etTitle);
+        et
+    }
+}
