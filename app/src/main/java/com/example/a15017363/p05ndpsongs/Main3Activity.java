c2405f2b6b0cc84a81f99b629198c28d4f46421f
dennywongso <15017096@myrp.edu.sg>
2017-05-18 13:53:44 +0800

    song class and main3activity

--- app/src/main/java/com/example/a15017363/p05ndpsongs/Main3Activity.java
+++ app/src/main/java/com/example/a15017363/p05ndpsongs/Main3Activity.java
@@ -1,22 +1,72 @@
 package com.example.a15017363.p05ndpsongs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
-    EditText etId, etTitle, etSinger, etYear;
+    EditText etTitle, etSinger, etYear;
+    TextView tvId;
     Button btnUpdate, btnDelete, btnCancel;
-    RadioGroup rgStars;
+    RadioButton rb1 , rb2, rb3, rb4, rb5;
+    Song data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        etId = (EditText)findViewById(R.id.etId);
+        tvId = (TextView)findViewById(R.id.tvId);
         etTitle = (EditText)findViewById(R.id.etTitle);
-        et
+        etSinger =(EditText)findViewById(R.id.etSinger);
+        etYear = (EditText)findViewById(R.id.etYear);
+        btnUpdate = (Button)findViewById(R.id.btnUpdate);
+        btnDelete = (Button)findViewById(R.id.btnDelete);
+        btnCancel = (Button)findViewById(R.id.btnCancel);
+
+        Intent i = getIntent();
+        data = (Song)i.getSerializableExtra("id");
+        tvId.setText(data.get_id());
+        etTitle.setText(data.getTitle());
+        etSinger.setText(data.getSingers());
+        etYear.setText(data.getYear());
+        int selected = data.getStars();
+        if(selected == 1){
+            rb1 = (RadioButton)findViewById(R.id.rb1);
+            rb1.setChecked(true);
+        } else if(selected == 2){
+            rb2 = (RadioButton)findViewById(R.id.rb2);
+            rb2.setChecked(true);
+        } else if(selected == 3){
+            rb3 = (RadioButton)findViewById(R.id.rb3);
+            rb3.setChecked(true);
+        } else if(selected == 4){
+            rb4 = (RadioButton)findViewById(R.id.rb4);
+            rb4.setChecked(true);
+        } else {
+            rb5 = (RadioButton)findViewById(R.id.rb5);
+            rb5.setChecked(true);
+        }
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                DBHelper dbh = new DBHelper(EditActivity.this);
+//                data.setNoteContent(etContent.getText().toString());
+//                dbh.updateNote(data);
+//                dbh.close();
+//                Intent i = new Intent();
+//                setResult(RESULT_OK,i);
+//                finish();
+
+            }
+        });
+
+
+
     }
 }
